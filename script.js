f0e9eec9488c0e1ba2fe24299649fae9e779eac8
Mija Dilberovic <dilberovicm36@gmail.com>
2025-04-18 11:09:42 -0700

    added css and js to validate the form

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ const sumButton = document.getElementById("sum");
 if (sumButton) {
     sumButton.addEventListener("click", calculateSum);
 }
+
+document.getElementById("contactForm").addEventListener('submit', function(e){
+    e.preventDefault()
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value
+    let message = document.getElementById("message").value
+
+    if (name === "" || email === "" || message === ""){
+        alert("Please fill out all the fields.")
+    }
+    else{
+        
+    }
+})
